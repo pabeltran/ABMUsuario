f49032b621c525bdf0995dad4c7226802b23eb6b
pbeltran <pbeltran@PHY-NB-INF-23>
2024-06-01 19:27:35 -0300

    Agregar archivos de proyecto.

---
+++ Data/FundacionContext.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using ABM_Usuario.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ABM_Usuario.Data;
+
+public partial class FundacionContext : DbContext
+{
+    public FundacionContext()
+    {
+    }
+
+    public FundacionContext(DbContextOptions<FundacionContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__USUARIO__3214EC271254387D");
+
+            entity.ToTable("USUARIO");
+
+            entity.HasIndex(e => e.Email, "UQ__USUARIO__161CF724280890DE").IsUnique();
+
+            entity.HasIndex(e => e.Usuario1, "UQ__USUARIO__E251F484AE6C152F").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("APELLIDO");
+            entity.Property(e => e.Ciudad)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("CIUDAD");
+            entity.Property(e => e.CodigoPostal)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("CODIGO_POSTAL");
+            entity.Property(e => e.Contraseña)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("CONTRASEÑA");
+            entity.Property(e => e.Direccion)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("DIRECCION");
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("EMAIL");
+            entity.Property(e => e.FechaNac)
+                .HasColumnType("date")
+                .HasColumnName("FECHA_NAC");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("NOMBRE");
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("TELEFONO");
+            entity.Property(e => e.Usuario1)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("USUARIO");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ABM_Usuario.Models;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string Usuario1 { get; set; } = null!;
+
+    public string Contraseña { get; set; } = null!;
+
+    public string? Nombre { get; set; }
+
+    public string Apellido { get; set; } = null!;
+
+    public string Direccion { get; set; } = null!;
+
+    public DateTime? FechaNac { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string? Telefono { get; set; }
+
+    public string? Ciudad { get; set; }
+
+    public string? CodigoPostal { get; set; }
+}
